9f001f9a9129e1a0cec3f5aaa17af7bba78eefb3
Tino B <bomantino@gmail.com>
2023-05-19 18:16:59 +0300

    Task to add the data to snowflake created

--- dags/etl_dags.py
+++ dags/etl_dags.py
@@ -2,6 +2,8 @@ from datetime import datetime, timedelta
 from airflow.decorators import dag, task
 from airflow.operators.python import PythonOperator
 from airflow.contrib.operators.snowflake_operator import SnowflakeOperator
+from airflow.contrib.hooks.snowflake_hook import SnowflakeHook
+from sqlalchemy import create_engine
 import requests
 import pandas as pd
 
@@ -33,18 +35,39 @@ def pipeline():
         df = df.explode(["hourly.time", "hourly.temperature_2m", "hourly.relativehumidity_2m"], ignore_index=1)
         df = df.drop(["generationtime_ms", "timezone", "utc_offset_seconds", "timezone_abbreviation", "elevation", "hourly_units.time", "hourly_units.temperature_2m", "hourly_units.relativehumidity_2m"], axis=1)
         df["hourly.time"] = pd.to_datetime(df["hourly.time"], format="%Y-%m-%dT%H:%M")
+        df = df.rename(columns={"hourly.time": "date_time", "hourly.temperature_2m":"temperature", "hourly.relativehumidity_2m":"RH"})
         return df
+    
+    @task
+    def check_data(df):
+
+        if df.empty:
+            raise Exception("Dataframe is empty")
+        elif df.isna().sum().sum() > 0:
+            raise Exception("Dataframe contains Null values")
+        
+    @task
+    def df_to_snowflake(df):
+        
+        snowflake_hook = SnowflakeHook(snowflake_conn_id = "snowflake_default")
+        conn = snowflake_hook.get_uri()
+        engine = create_engine(conn)
+        df.to_sql("WEATHER_TABLE", con = engine, index = False, if_exists = "replace")
+
 
     task1 = get_weather_data()
 
-    task2 = SnowflakeOperator(
+    task2 = check_data(task1)
+
+    task3 = df_to_snowflake(task1)
+
+    task0 = SnowflakeOperator(
         task_id = "create_table",
         sql = "sql/create_table.sql",
         snowflake_conn_id = "snowflake_default"
     )
 
-   
 
-    task1 >> task2
+    task0 >> task1 >> task2 >> task3
 
 pipeline()
--- dags/sql/create_table.sql
+++ dags/sql/create_table.sql
@@ -2,6 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS weather_table (
     data_id int IDENTITY(1, 1),
+    date_time DATE,
     latitude FLOAT,
     longitude FLOAT,
     temperature FLOAT,
